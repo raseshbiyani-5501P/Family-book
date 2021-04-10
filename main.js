916a00a50f2b56905733f1195bcb171b8e869ceb
raseshbiyani-5501P <72201706+raseshbiyani-5501P@users.noreply.github.com>
2021-04-10 12:45:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var home =[
+    "Dad_character.png",
+    "Mom character.jpg",
+    "Sister character.jpg",
+    "My character.jpg"
+]
+var i=0;
+function next(){
+    document.getElementById("home").src=home[i]
+    i=i+1;
+}
